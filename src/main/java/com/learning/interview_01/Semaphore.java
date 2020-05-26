ba1f725d021923d17e3f521282f063ccf552b3e7
hujuqiang <18296154764@163.com>
2020-05-26 23:56:53 +0800

    添加面试题

---
+++ src/main/java/com/learning/interview_01/Semaphore.java
@@ -0,0 +1,4 @@
+package com.learning.interview_01;
+
+public class Semaphore {
+}
---
+++ src/main/java/com/learning/interview_02/Container1.java
@@ -0,0 +1,10 @@
+package com.learning.interview_02;
+
+/**
+ * 面试题：写一个固定容量同步容器，拥有put和get方法，以及getCount方法，
+ *  * 能够支持2个生产者线程以及10个消费者线程的阻塞调用
+ *  *
+ *  * 使用wait和notify/notifyAll来实现
+ */
+public class Container1 {
+}
