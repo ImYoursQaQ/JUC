7992abfa524841b070e89890e3ebd30d919f827c
hujuqiang <18296154764@163.com>
2020-05-31 16:11:04 +0800

    实现一个同步容器

--- src/main/java/com/learning/interview_02/Container1.java
+++ src/main/java/com/learning/interview_02/Container1.java
@@ -1,10 +1,73 @@
 package com.learning.interview_02;
 
+import java.util.LinkedList;
+import java.util.concurrent.TimeUnit;
+
 /**
  * 面试题：写一个固定容量同步容器，拥有put和get方法，以及getCount方法，
  *  * 能够支持2个生产者线程以及10个消费者线程的阻塞调用
  *  *
  *  * 使用wait和notify/notifyAll来实现
  */
-public class Container1 {
+public class Container1<T> {
+
+    private final LinkedList<T> list = new LinkedList<T>();
+    final private int MAX = 10; //最多10个元素
+    private int count = 0;
+
+    public synchronized void put(T o) {
+        while (getCount() == MAX) { //想想为什么用while而不是用if？
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        list.add(o);
+        count++;
+        notifyAll();//通知消费者线程进行消费
+    }
+
+    public synchronized T get() {
+        while (getCount() == 0) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        T t = (T) list.removeFirst();
+        count--;
+        notifyAll();//通知生产者进行生产
+        return t;
+    }
+
+    public synchronized int getCount() {
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Container1 container1 = new Container1<String>();
+        //启动消费者线程
+        for(int i = 0; i < 10; i ++) {
+            new Thread(() -> {
+                for(int j = 0; j < 5; j ++) {
+                    System.out.println(container1.get());
+                }
+            }, "C" + i).start();
+        }
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        //启动生产者线程
+        for(int i = 0; i < 2; i ++) {
+            new Thread(() -> {
+                for(int j = 0; j < 25; j ++) {
+                    container1.put(Thread.currentThread().getName() + " " + j);
+                }
+            }, "P" + i).start();
+        }
+    }
 }
--- src/main/java/com/learning/interview_02/Container2.java
+++ src/main/java/com/learning/interview_02/Container2.java
@@ -1,5 +1,10 @@
 package com.learning.interview_02;
 
+import java.util.LinkedList;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * 面试题：写一个固定容量同步容器，拥有put和get方法，以及getCount方法，
  *  * 能够支持2个生产者线程以及10个消费者线程的阻塞调用
@@ -9,5 +14,72 @@ package com.learning.interview_02;
  *  * 使用Lock和Condition来实现
  *  * 对比两种方式，Condition的方式可以更加精确的指定哪些线程被唤醒
  */
-public class Container2 {
+public class Container2<T> {
+    private LinkedList<T> list = new LinkedList<>();
+    final private int MAX = 10; //最多10个元素
+    private int count = 0;
+
+    private ReentrantLock lock = new ReentrantLock();
+    private Condition condition1 = lock.newCondition();
+    private Condition condition2 = lock.newCondition();
+
+    public void put(T t) {
+        try {
+            lock.lock();
+            while (count == MAX) { //想想为什么用while而不是用if？
+                condition1.await();
+            }
+            list.add(t);
+            count ++;
+            condition2.signalAll(); //通知消费者线程进行消费
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public T get() {
+        T t = null;
+        try {
+            lock.lock();
+            while(count == 0) {
+                System.out.println(Thread.currentThread().getName() + "await");
+                condition2.await();
+            }
+            t = list.removeFirst();
+            count --;
+            condition1.signalAll();//通知生产者进行生产
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+        return t;
+    }
+
+    public static void main(String[] args) {
+        Container2 container2 = new Container2<String>();
+        //启动消费者线程
+        for(int i = 0; i < 10; i ++) {
+            new Thread(() -> {
+                for(int j = 0; j < 5; j ++) {
+                    System.out.println(container2.get());
+                }
+            }, "C" + i).start();
+        }
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        //启动生产者线程
+        for(int i = 0; i < 2; i ++) {
+            new Thread(() -> {
+                for(int j = 0; j < 25; j ++) {
+                    container2.put(Thread.currentThread().getName() + " " + j);
+                }
+            }, "P" + i).start();
+        }
+    }
 }
