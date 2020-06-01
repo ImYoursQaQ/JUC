4b23bdb2db08b8dfa70eb6b824d2971f74e02bad
hujuqiang <18296154764@163.com>
2020-06-01 22:19:21 +0800

    temp

--- src/main/java/com/learning/collection/TransferQueue_01.java
+++ src/main/java/com/learning/collection/TransferQueue_01.java
@@ -1,21 +1,24 @@
 package com.learning.collection;
 
 import java.util.concurrent.LinkedTransferQueue;
+import java.util.concurrent.TimeUnit;
 
 public class TransferQueue_01 {
 
 	public static void main(String[] args) throws InterruptedException {
 		LinkedTransferQueue<String> strs = new LinkedTransferQueue<>();
 
-		/*new Thread(() -> {
+		new Thread(() -> {
 			try {
+				TimeUnit.SECONDS.sleep(10);
 				System.out.println(strs.take());
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-		}).start();*/
+		}).start();
 
-		//strs.transfer("aaa");
+		strs.transfer("aaa");// 线程阻塞，等待消费者消费
+		System.out.println("ccc");
 
 		strs.put("aaab");
 
